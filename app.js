4668fcbc53d5bd1176b71b184dbd1f662acea893
Khizar <shaikh.muhammad.khizar@gmail.com>
2021-06-23 16:53:48 +0500

    Add some functions

--- app.js
+++ app.js
@@ -1,132 +1,165 @@
-var obj = {
-    question1 : {
-        question : "Html stands for",
-        option1 : "Hyper text Markup Language",
-        option2 : "Hyper text Markup Language 1",
-        option3 : "Hyper text Markup Language 2",
-        option4 : "Hyper text Markup Language 3",
-        ans : "Hyper text Markup Language"
+var questions = [
+    {
+        question: "Html stands for",
+        option1: "Hyper text Markup Language",
+        option2: "Hyper text Markup Language 1",
+        option3: "Hyper text Markup Language 2",
+        option4: "Hyper text Markup Language 3",
+        ans: "ans1"
     },
-    question2 : {
-        question : "Html stands for 1",
-        option1 : "Hyper text Markup Language",
-        option2 : "Hyper text Markup Language 1",
-        option3 : "Hyper text Markup Language 2",
-        option4 : "Hyper text Markup Language 3",
-        ans : "Hyper text Markup Language"
+    {
+        question: "Html stands for 1",
+        option1: "Hyper text Markup Language",
+        option2: "Hyper text Markup Language 1",
+        option3: "Hyper text Markup Language 2",
+        option4: "Hyper text Markup Language 3",
+        ans: "ans3"
     },
-    question3 : {
-        question : "Html stands for 2",
-        option1 : "Hyper text Markup Language",
-        option2 : "Hyper text Markup Language 1",
-        option3 : "Hyper text Markup Language 2",
-        option4 : "Hyper text Markup Language 3",
-        ans : "Hyper text Markup Language"
+    {
+        question: "Html stands for 2",
+        option1: "Hyper text Markup Language",
+        option2: "Hyper text Markup Language 1",
+        option3: "Hyper text Markup Language 2",
+        option4: "Hyper text Markup Language 3",
+        ans: "ans2"
     },
-    question4 : {
-        question : "Html stands for 3",
-        option1 : "Hyper text Markup Language",
-        option2 : "Hyper text Markup Language 1",
-        option3 : "Hyper text Markup Language 2",
-        option4 : "Hyper text Markup Language 3",
-        ans : "Hyper text Markup Language"
+    {
+        question: "Html stands for 3",
+        option1: "Hyper text Markup Language",
+        option2: "Hyper text Markup Language 1",
+        option3: "Hyper text Markup Language 2",
+        option4: "Hyper text Markup Language 3",
+        ans: "ans4"
     },
-    question5 : {
-        question : "Html stands for 4",
-        option1 : "Hyper text Markup Language",
-        option2 : "Hyper text Markup Language 1",
-        option3 : "Hyper text Markup Language 2",
-        option4 : "Hyper text Markup Language 3",
-        ans : "Hyper text Markup Language"
+    {
+        question: "Html stands for 4",
+        option1: "Hyper text Markup Language",
+        option2: "Hyper text Markup Language 1",
+        option3: "Hyper text Markup Language 2",
+        option4: "Hyper text Markup Language 3",
+        ans: "ans2"
     }
-}
-var username = document.getElementById('username');
-var password = document.getElementById('password');
+]
+var body = document.getElementById('body');
+let uName = document.getElementById('username');
+var pass = document.getElementById('password');
 var question = document.getElementById('ques');
 var option1 = document.getElementById('option1');
 var option2 = document.getElementById('option2');
 var option3 = document.getElementById('option3');
 var option4 = document.getElementById('option4');
-var opt1 = document.getElementById('opt1');
-var opt2 = document.getElementById('opt2');
-var opt3 = document.getElementById('opt3');
-var opt4 = document.getElementById('opt4');
-var next = document.getElementById('next');
-// var score = document.getElementById('score');
+var nextBtn = document.getElementById('nextBtn');
+var questionNo = document.getElementById('currentNo');
+var totalquestions = document.getElementById('totalNo');
+var minutes = document.getElementById('min');
+var seconds = document.getElementById('sec');
+var quizbox = document.getElementById('quiz');
+var mainbox = document.getElementById('main');
+totalquestions.innerHTML = questions.length;
+var questionCount = 0;
+var marks = 0;
 // function login(){
-//     if(username === "smkhizar"){
-//         if(password === 123){
+//     if(name === "smkhizar"){
+//         if(pass === 123){
 //             login.className += "";
 //         }
 //     }
 
 // }
-
-// var i;
-// var score = 0;
-function quiz(){
-    // i = 1;
-    question.innerHTML = obj.question1.question;
-    option1.innerHTML = obj.question1.option1;
-    option2.innerHTML = obj.question1.option2;
-    option3.innerHTML = obj.question1.option3;
-    option4.innerHTML = obj.question1.option4;
-}
-function next(){
-    alert("Correct Answer!");
-
-    // if(opt1.checked){
-    //     if(option1.innerHTML === obj.question1.ans){
-    //         alert("Correct Answer!");
-    //     } 
-    // }
-    // switch(i){
-    //     case 1:
-
-    //         question.innerHTML = obj.question2.question;
-    //         option1.innerHTML = obj.question2.option1;
-    //         option2.innerHTML = obj.question2.option2;
-    //         option3.innerHTML = obj.question2.option3;
-    //         option4.innerHTML = obj.question2.option4;
-    // score.innerHTML = score;
-    //         break;
-    //     case 2:
-    //         question.innerHTML = obj.question3.question;
-    //         option1.innerHTML = obj.question3.option1;
-    //         option2.innerHTML = obj.question3.option2;
-    //         option3.innerHTML = obj.question3.option3;
-    //         option4.innerHTML = obj.question3.option4;
-    // score.innerHTML = score;
-            
-    //         break;
-    //     case 3:
-    //         question.innerHTML = obj.question4.question;
-    //         option1.innerHTML = obj.question4.option1;
-    //         option2.innerHTML = obj.question4.option2;
-    //         option3.innerHTML = obj.question4.option3;
-    //         option4.innerHTML = obj.question4.option4;
-    // score.innerHTML = score;
-            
-    //         break;
-    //     case 4:
-    //         question.innerHTML = obj.question5.question;
-    //         option1.innerHTML = obj.question5.option1;
-    //         option2.innerHTML = obj.question5.option2;
-    //         option3.innerHTML = obj.question5.option3;
-    //         option4.innerHTML = obj.question5.option4;
-    // score.innerHTML = score;
-            
-    //         break;
-    // }
-    // i++;
-}
-var sec = 00;
-var min = 10;
-var a = setInterval(() => {
-    if(sec === 0){
-        min--;
-        sec = 60;
+function check(){
+    var opt1 = document.getElementById('ans1');
+    var opt2 = document.getElementById('ans2');
+    var opt3 = document.getElementById('ans3');
+    var opt4 = document.getElementById('ans4');
+    console.log(questions[questionCount].ans);
+    switch(questions[questionCount].ans){
+        case "ans1":
+            console.log(opt1.checked);
+            if(opt1.checked){
+                marks++;
+            }
+            break;
+        case "ans2":
+            console.log(opt2.checked);
+            if(opt2.checked){
+                marks++;
+            }
+                break;
+        case "ans3":
+            console.log(opt3.checked);
+            if(opt3.checked){
+                marks++;
+            }
+            break;
+        case "ans4":
+            if(opt4.checked){
+                marks++;
+            }
+            break;
     }
-    sec--;
-}, 1000);
-console.log(a);
+}
+function start() {
+    mainbox.innerHTML = '<h1>Quiz Application</h1><div class="login" id="login"><button onclick="text()" class="btn-primary btn">Start Quiz</button></div>';
+}
+function text(){
+    mainbox.setAttribute('class','hidden');
+    quizbox.setAttribute('class','col-6 quiz');
+}
+function timer() {
+    var sec = 00;
+    var min = 10;
+    var a = setInterval(function () {
+        if (sec === 0) {
+            --min;
+            sec = 60;
+        }
+        --sec;
+        if (min === 00 && sec === 00) {
+            clearInterval(a);
+            submit();
+        }
+        minutes.innerHTML = min;
+        seconds.innerHTML = sec;
+        // console.log(min,sec);
+    }, 1000);
+}
+function quiz() {
+    var questionList = questions[questionCount];
+    question.innerHTML = questionList.question;
+    option1.innerHTML = questionList.option1;
+    option2.innerHTML = questionList.option2;
+    option3.innerHTML = questionList.option3;
+    option4.innerHTML = questionList.option4;
+    timer();
+    questionNo.innerHTML = (questionCount + 1);
+}
+function submit() {
+    check();
+    quizbox.setAttribute('class', 'resultBox');
+    var greeting;
+    if (marks >= 3) {
+        greeting = "Congratulations ";
+    }
+    else {
+        greeting = "Sorry ";
+    }
+    quizbox.innerHTML = '<div class="header"><div><h1>Result</h1></div></div><div class="result">' + greeting + '</br>Your score is ' + marks + '</div>';
+}
+function nextQuestion() {
+    check();
+    if (questionCount < questions.length) {
+        questionCount++;
+        var questionList = questions[questionCount];
+        question.innerHTML = questionList.question;
+        option1.innerHTML = questionList.option1;
+        option2.innerHTML = questionList.option2;
+        option3.innerHTML = questionList.option3;
+        option4.innerHTML = questionList.option4;
+        questionNo.innerHTML = (questionCount + 1);
+        if (questionCount === questions.length - 1) {
+            nextBtn.innerHTML = "Submit";
+            nextBtn.setAttribute('onclick', 'submit()');
+        }
+    }
+}
+quiz();
